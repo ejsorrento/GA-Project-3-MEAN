bb52434b46fbd1b5b723a85f9cd31e05acc6ff16
EJ Sorrento <ejsorrento@gmail.com>
2016-12-03 14:22:15 -0500

    HBS installed, main frontend page songs-index working

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 var express = require("express");
+var parser = require("body-parser");
+var hbs = require("express-handlebars");
+var mongoose = require("./db/schema");
 var app = express();
 
+var Song = mongoose.model("Song");
+
+app.set("port", process.env.PORT || 3001);
+app.set('view engine', 'hbs');
+app.engine(".hbs", hbs({
+  extname:        ".hbs",
+  partialsDir:    "views/",
+  layoutsDir:     "views/",
+  defaultLayout:  "layout-main"
+}));
+app.use("/assets", express.static("public"));
+app.use(parser.json({extended: true}));
 
 app.get("/", (req, res) => {
-  res.send("QWERTY");
+res.render("songs-index");
 });
 
-
+app.get("/api/songs", (req, res) => {
+  Song.find({}).then(function(songs){
+  res.json();
+});
+});
 
 app.listen(3001, () => {
   console.log("app listening");
