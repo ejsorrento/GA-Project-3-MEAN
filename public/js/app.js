d53137730b27e23b098dbaa4df7005d6d21ace09
Jonathan Reiser <jdreiser1@gmail.com>
2016-12-04 18:04:54 -0500

    Piano appears on front page, trying to connecting as directive

--- public/js/app.js
+++ public/js/app.js
@@ -1,67 +1,82 @@
-angular.module("diddy", [
-  "ui.router",
-  "ngResource"
-])
-.config(["$stateProvider",
-  RouterFunction
-])
-.controller("songsIndexController", [
-  "Song",
-  "$state",
-  songsIndexControllerFunction
-])
-.controller("songsShowController", [
-  "$state",
-  "$stateParams",
-  "Song",
-  songsShowControllerFunction
-])
-.factory("Song", [
-  "$resource",
-  SongFactory
-]);
+angular
+    .module("diddy", [
+        "ui.router",
+        "ngResource"
+    ])
+    .config(["$stateProvider",
+        RouterFunction
+    ])
+    .controller("songsIndexController", [
+        "Song",
+        "$state",
+        songsIndexControllerFunction
+    ])
+    .controller("songsShowController", [
+        "$state",
+        "$stateParams",
+        "Song",
+        songsShowControllerFunction
+    ])
+    .factory("Song", [
+        "$resource",
+        SongFactory
+    ]);
+
+// function Piano() {
+//   template: '<h1>Hi There!</h1>'
+// }
 
 function SongFactory($resource) {
-  return $resource("/api/songs/:name", {}, {
-    update: {method: "PUT"}
-  });
+    return $resource("/api/songs/:name", {}, {
+        update: {
+            method: "PUT"
+        }
+    });
 }
 
 function songsIndexControllerFunction(Song, $state) {
-  this.songs = Song.query();
-  this.newSong = new Song();
-  this.create = function() {
-    this.newSong.$save().then(function(song) {
-      $state.go("show", { name: song.name});
-    });
-  };
-}
-function songsShowControllerFunction($state, $stateParams, Song) {
-  this.song = Song.get({name:
-    $stateParams.name});
-  this.update = function() {
-    this.song.$update({name: $stateParams.name});
-  };
-  this.destroy = function() {
-    this.song.$delete({name:
-      $stateParams.name}).then(function() {
-        $state.go("index");
-      });
-  };
+    this.songs = Song.query();
+    this.newSong = new Song();
+    this.create = function() {
+        this.newSong.$save().then(function(song) {
+            $state.go("show", {
+                name: song.name
+            });
+        });
+    };
+    console.log("Hello")
 }
 
-function RouterFunction ($stateProvider) {
-  $stateProvider
-  .state("index", {
-    url: "/",
-    templateUrl: "/assets/js/ng-views/index.html",
-    controller: "songsIndexController",
-    controllerAs: "vm"
-  })
-  .state("show", {
-    url: "/songs/:name",
-    templateUrl: "/assets/js/ng-views/show.html",
-    controller: "songsShowController",
-    controllerAs: "vm"
-  });
+function songsShowControllerFunction($state, $stateParams, Song) {
+    this.song = Song.get({
+        name: $stateParams.name
+    });
+    this.update = function() {
+        this.song.$update({
+            name: $stateParams.name
+        });
+    };
+    this.destroy = function() {
+        this.song.$delete({
+            name: $stateParams.name
+        }).then(function() {
+            $state.go("index");
+        });
+    };
+}
+
+function RouterFunction($stateProvider) {
+    $stateProvider
+        .state("index", {
+            url: "/",
+            templateUrl: "/assets/js/ng-views/index.html",
+            controller: "songsIndexController",
+            controllerAs: "vm"
+        })
+        .state("show", {
+            url: "/songs/:name",
+            templateUrl: "/assets/js/ng-views/show.html",
+            controller: "songsShowController",
+            controllerAs: "vm"
+        });
 }
