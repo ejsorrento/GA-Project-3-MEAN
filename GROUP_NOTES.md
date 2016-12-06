7ec80d6473eb328ebdbfdb79ef1b030826a9f744
EJ Sorrento <ejsorrento@gmail.com>
2016-12-06 11:50:37 -0500

    Revert "Styling"

--- GROUP_NOTES.md
+++ GROUP_NOTES.md
@@ -7,14 +7,4 @@ challenges and anything else**
 
 * Kevin -
 
-* EJ -
-12/3/2016:
-Worked @GA-
--Sam and Jon divided and calibrated what front end responsibilities they'd take
-while I focused on nesting CRUD on the back-end
--Finished defining full CRUD for Songs model
--Created files pertaining to Songs model CRUD and also had two different files
-for the beginning of nesting 'possible_app.js, possible-index.js'
---Messaged the group my code at the end of my session so could collaborate on my
-work and get their feedback on my direction, hoping that I followed the defined
-ideas we had
+* EJ - 
--- index.js
+++ index.js
@@ -1,59 +1,12 @@
 var express = require("express");
-var parser = require("body-parser");
-var hbs = require("express-handlebars");
-var mongoose = require("./db/schema");
 var app = express();
 
-var Song = mongoose.model("Song");
 
-app.set("port", process.env.PORT || 3001);
-app.set('view engine', 'hbs');
-app.engine(".hbs", hbs({
-  extname:        ".hbs",
-  partialsDir:    "views/",
-  layoutsDir:     "views/",
-  defaultLayout:  "layout-main"
-}));
-app.use("/assets", express.static("public"));
-app.use(parser.json({extended: true}));
-
-app.get("/", function(req, res) {
-res.render("songs-index");
+app.get("/", (req, res) => {
+  res.send("QWERTY");
 });
 
-app.get("/api/songs", function(req, res) {
-  Song.find({}).then(function(songs){
-  res.json(songs);
-});
-});
 
-app.get("/api/songs/:name", function(req, res) {
-  Song.findOne({name:
-    req.params.name}).then(function(song) {
-    res.json(song);
-  });
-});
-
-app.post("/api/songs", function(req, res) {
-  Song.create(req.body).then(function(song){
-    res.json(song);
-  });
-});
-
-app.put("/api/songs/:name", function(req, res) {
-  Song.findOneAndUpdate({name:
-  req.params.name}, req.body, {new: true}).then(function(song) {
-    res.json(song);
-  });
-});
-
-app.delete("/api/songs/:name", function(req, res) {
-  Song.findOneAndRemove({name:
-    req.params.name}).then(function() {
-    res.json({ success: true });
-  });
-});
-// app.get("/api/album/songs/:id"
 
 app.listen(3001, () => {
   console.log("app listening");
